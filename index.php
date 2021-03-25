453904c7843f71dc35c9649b55f3e495b577ed5a
IslamKurbanov <61545789+IslamKurbanov@users.noreply.github.com>
2021-03-25 13:26:49 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<!--
+	Rocket Internet by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<html>
+<head>
+	<title>Islam Kurbanov-Web Developer</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+	<!-- Goggle Font -->
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+
+	<!-- Font Css -->
+	<link rel="stylesheet" type="text/css" href="font-awesome.min.css">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<!-- Animation Css -->
+	<link rel="stylesheet" href="animate.css">
+
+</head>
+<body>
+<header>
+	<div id="homeFullScreen">
+		<div class="header-text">
+			<h1>Islam Kurbanov</h1>
+			<div class="vertical-line"></div>
+			<p>
+				The odyssey of full stack developer
+				<br />
+				I ‏׳‏‎m ‎ a web developer 
+			</p>
+		</div><!-- End HeaderText -->
+	</div><!-- End homeFullScreen -->
+</header><!-- End Header -->
+
+<!-- Feature -->
+
+<section class="feature">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="feature-box">
+					<h1>About me</h1>
+					<p>I am frontend developer.I like dabbling in various parts of frontend development and like to learn about new technologies, and process of coding.</p>
+				</div>
+			</div><!-- End col-md-6 -->
+			<div class="col-md-6 col-sm-6 nopadding">
+				<img src="images/1.png">
+			</div><!-- End col-md-6 -->
+
+			<div class="clearfix"></div><!-- End clearfix -->
+
+			<div class="col-md-6 col-sm-6 nopadding">
+				<img src="./img5.jpg">
+			</div><!-- End col-md-6 -->
+			<div class="col-md-6 col-sm-6">
+				<div class="feature-box">
+					<h1></h1>
+					<p>I use technologies such as HTML, CSS, JavaScript, React, Angular, Bootstrap.</p>
+				</div>
+			</div><!-- End col-md-6 -->
+		</div>
+	</div>
+</section>
+
+<!-- portfolio -->
+
+<section class="portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="heading">Portfolio</h1>
+			</div><!-- End col-md-12 -->
+
+			<div class="col-md-4 col-sm-4">
+				<a href="https://urbantemp.herokuapp.com/"><img src="./img1.png" alt="img"></a>
+			</div><!-- End col-md-4 -->
+			<div class="col-md-4 col-sm-4">
+				<a href="https://animatedbubbles.herokuapp.com/"><img src="./img2.png" alt="img"></a>
+			</div><!-- End col-md-4 -->
+			<div class="col-md-4 col-sm-4 last-img">
+				<a href="#"><img src="./img3.png" alt="img"></a>
+			</div><!-- End col-md-4 -->
+
+			<div class="clearfix"></div><!-- End clearfix -->
+
+			<div class="col-md-4 col-sm-4">
+				<img src="./p-4.png">
+			</div><!-- End col-md-4 -->
+			<div class="col-md-4 col-sm-4">
+				<img src="./p-5.png">
+			</div><!-- End col-md-4 -->
+			<div class="col-md-4 col-sm-4 last-img">
+				<img src="./p-6.png">
+			</div><!-- End col-md-4 -->
+		</div><!-- End row -->
+	</div><!-- End container -->
+</section>
+
+<!-- Call to Action -->
+<section class="call-top-action text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Any type of Query & Discussion</h1>
+				<p>It`s certainly no way to get noticed in today`s marketplace. 
+				<br />
+				Quality content marketing is what it takes to make a modern brand truly shines</p>
+				<span>Let`s talk with me</span>
+			</div><!-- End col-md-12 -->
+		</div><!-- End row -->
+	</div><!-- End container -->
+</section>
+
+<!-- Footer -->
+<footer>
+	<ul class="in-line">
+		<li><a href="#"><span><i class="fa fa-facebook"></i></span></a></li>
+		<li><a href="#"><span><i class="fa fa-twitter"></i></span></a></li>
+		<li><a href="#"><span><i class="fa fa-youtube"></i></span></a></li>
+	</ul>
+	<span class="copyright">&#169; All rights reserved</span>
+</footer>
+	
+
+
+
+<!-- Main JS -->
+<script type="text/javascript" src="jquery-main.js"></script>
+
+<!-- Bootstrap JS -->
+<script type="text/javascript" src="bootstrap.min.js"></script>
+
+<!-- Animation JS -->
+<script type="text/javascript" src="wow.min.js"></script>
+
+<!-- Custom JS -->
+<script type="text/javascript" src="custom.js"></script>
+
+
+</body>
+</html>
